cb0cf0fb63b34eef74fd693cb4a5ddbff7362eaf
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2019-04-13 23:20:16 +0530

    Updated Ava Setup

--- test/helpers/setup.js
+++ test/helpers/setup.js
@@ -1,12 +1,25 @@
-require('browser-env')()
-const hooks = require('require-extension-hooks')
-const Vue = require('vue')
+const hooks = require('require-extension-hooks');
 
-Vue.config.productionTip = false
+if (process.env.TEST === 'unit') {
+  require('jsdom-global')();
+  require('browser-env');
+  const Vue = require('vue');
+  Vue.config.productionTip = false;
+  // https://github.com/nuxt/create-nuxt-app/issues/180#issuecomment-463069941
+  window.Date = global.Date = Date;
+}
 
+if (process.env.TEST === 'e2e') {
+  const Vue = require('vue');
+  Vue.config.productionTip = false;
+}
+
+// Setup vue files to be processed by `require-extension-hooks-vue`
 hooks('vue')
   .plugin('vue')
-  .push()
+  .push();
+// Setup vue and js files to be processed by `require-extension-hooks-babel`
 hooks(['vue', 'js'])
+  .exclude(({ filename }) => filename.match(/\/node_modules\//))
   .plugin('babel')
-  .push()
+  .push();
