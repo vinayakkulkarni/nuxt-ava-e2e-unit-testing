0a31eb3aa7299a8b58d54abe34a9a1e93ff6895d
Wagner Souza <wagner.souza@accenture.com>
2019-05-22 13:47:05 -0300

    creates example page with vuex

---
+++ pages/examples/vuex-todo.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  components: {
+    Todo: () => import('~/components/Todo.vue'),
+  },
+};
+</script>
+
+<template>
+  <section id="vuex-example">
+    <div class="container">
+      <header class="vuex-example-header">
+        <h2>This page is for vuex testing example</h2>
+      </header>
+      <div class="vuex-example-content">
+        <Todo />
+      </div>
+    </div>
+  </section>
+</template>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  max-width: 70%;
+  margin: 0 auto;
+}
+.vuex-example-header {
+  margin-top: 2rem;
+}
+.done {
+  text-decoration: line-through;
+}
+</style>
