e03977e729c9a3e2c76f95edc14831d2eae85fc7
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2021-02-22 02:23:02 +0530

    style: update eslint config & css style

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,25 +2,22 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   extends: [
     'plugin:vue/recommended',
     'plugin:nuxt/recommended',
     'plugin:prettier/recommended',
-    'prettier'
+    'prettier',
   ],
   // required to lint *.vue files
-  plugins: [
-    'vue',
-    'prettier'
-  ],
+  plugins: ['vue', 'prettier'],
   // add your custom rules here
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
-}
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+};
